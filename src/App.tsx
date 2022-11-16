a9bd6e956364649e74bd52be1172d5a249b55261
rojanbade <rojan.bade@rumsan.com>
2022-11-16 13:37:41 +0545

    login page ui updated, user's ledger detail added

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import CustomLayout from "./layout/customLayout";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 // import {Route} from "react-router-dom"
 import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
-import authProvider from  "./utils/authProvider";
+import authProvider from "./utils/authProvider";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Transactions from "./components/transactions/list";
@@ -30,7 +30,7 @@ import { EditUser } from "./components/users/edit/EditUser";
 import { myDataProvider } from "./utils/data-provider";
 import { DetailUser } from "./components/users/detail";
 
-// import LoginPage from './components/login/login';
+import LoginPage from "./components/login/login";
 
 function App() {
   return (
@@ -41,6 +41,7 @@ function App() {
         dataProvider={myDataProvider}
         authProvider={authProvider}
         layout={CustomLayout}
+        loginPage={LoginPage}
       >
         <Resource
           name="categories"
--- src/utils/authProvider.tsx
+++ src/utils/authProvider.tsx
@@ -4,88 +4,88 @@ import axios from "axios";
 import decodeJwt from "jwt-decode";
 
 // const servicesHost = "http://rayyapi.tk";
-import {servicesHost} from "./data-provider"
+import { servicesHost } from "./data-provider";
 
 const authProvider = {
-    login: ({ username, password }:{username:string,password:string}) => {
+  login: ({ email, password }: { email: string; password: string }) => {
+    const request = {
+      method: "POST",
+      url: `${servicesHost}/auth/signin`,
+      data: {
+        email,
+        password,
+      },
+      headers: new Headers({ "Content-Type": "application/json" }),
+    };
+    return axios(request)
+      .then((response) => {
+        // console.log("response --> " , response)
 
-        const request = {
-            method: "POST",
-            url:`${servicesHost}/auth/signin`,
-            data: {
-              email:username,
-              password,
-            },
-            headers: new Headers({ "Content-Type": "application/json" }),
+        if (response?.data?.tokens) {
+          const refreshToken = response?.data?.tokens?.refreshToken;
+
+          const userId = response?.data?.uid;
+          const email = response?.data?.name ? response?.data?.name : "user";
+
+          const decodedToken = decodeJwt(response?.data?.tokens?.refreshToken);
+
+          const user = {
+            refreshToken,
+            userId,
+            email,
+            permission: decodedToken.roles,
           };
-          return axios(request)
-          .then((response) => {
-            // console.log("response --> " , response)
 
-            if (response?.data?.tokens) {
-              const refreshToken = response?.data?.tokens?.refreshToken;
-              
-              const userId = response?.data?.uid;
-              const username = response?.data?.name ? response?.data?.name :"user"
+          // console.log("decode access token " , decodedToken)
 
-              const decodedToken = decodeJwt(response?.data?.tokens?.refreshToken);
+          localStorage.setItem("user", JSON.stringify(user));
 
-
-              const user = {
-                refreshToken,
-                userId,
-                username,
-                permission:decodedToken.roles
-              }
-
-            // console.log("decode access token " , decodedToken)
-
-              localStorage.setItem("user", JSON.stringify(user));             
-
-              if (response.status < 200 || response.status >= 300) {
-                throw new Error(response.statusText);
-              }
-            } else {
-                throw new Error("Something went wrong.Please Try again Sometimes Later.");
-            }
-    
-            // return response?.data?.json();
-          })
-          .catch((err) => { 
-            const status = error.status;
-        if (status === 401 || status === 403) {
-          throw new Error("Please enter valid Email or Password.")  
+          if (response.status < 200 || response.status >= 300) {
+            throw new Error(response.statusText);
+          }
+        } else {
+          throw new Error(
+            "Something went wrong.Please Try again Sometimes Later."
+          );
         }
-            throw new Error("Something went wrong.Please Try again Sometimes Later.")   
-          });
 
-    },
-    logout: () => {
-        localStorage.removeItem('user');
-        return Promise.resolve();
-    },
-    checkAuth: () =>
-        localStorage.getItem('user') ? Promise.resolve() : Promise.reject(),
-    checkError:  (error:any) => {
+        // return response?.data?.json();
+      })
+      .catch((err) => {
         const status = error.status;
         if (status === 401 || status === 403) {
-            localStorage.removeItem('user');
-            return Promise.reject();
+          throw new Error("Please enter valid Email or Password.");
         }
-        // other error code (404, 500, etc): no need to log out
-        return Promise.resolve();
-    },
-    getIdentity: () =>{
-        const user:any=JSON.parse(localStorage.getItem("user"))
-        // console.log("user",user)
+        throw new Error(
+          "Something went wrong.Please Try again Sometimes Later."
+        );
+      });
+  },
+  logout: () => {
+    localStorage.removeItem("user");
+    return Promise.resolve();
+  },
+  checkAuth: () =>
+    localStorage.getItem("user") ? Promise.resolve() : Promise.reject(),
+  checkError: (error: any) => {
+    const status = error.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem("user");
+      return Promise.reject();
+    }
+    // other error code (404, 500, etc): no need to log out
+    return Promise.resolve();
+  },
+  getIdentity: () => {
+    const user: any = JSON.parse(localStorage.getItem("user"));
+    // console.log("user",user)
 
- 
-       return  Promise.resolve({
-             id: user.userId,
-             fullName:user.username,
-         })
-    },
-    getPermissions: () => Promise.resolve(''),
+    return Promise.resolve({
+      id: user.userId,
+      fullName: user.email,
+    });
+  },
+  getPermissions: () => Promise.resolve(""),
 };
 
 export default authProvider;
