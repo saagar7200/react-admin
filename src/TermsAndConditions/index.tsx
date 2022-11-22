be3a16636cb42c14dd9f99ce1797d10f8d902b5d
sagar <rsaagar7200@gmail.com>
2022-11-22 10:28:07 +0545

    unused tag removed

--- src/TermsAndConditions/index.tsx
+++ src/TermsAndConditions/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./terms.css";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
