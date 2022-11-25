e2eb8e70ed4996fb6f0807c3cc83551343e6999e
sagar <rsaagar7200@gmail.com>
2022-11-25 15:27:30 +0545

    console removed from data-provider

--- src/utils/data-provider.tsx
+++ src/utils/data-provider.tsx
@@ -114,8 +114,6 @@ export const myDataProvider = {
   },
 
   create: (resource: any, params: any) => {
-    console.log("create", { params });
-
     const objectFromForm = params.data;
     let isFormWithFile = false;
     //go through each value in object, and if a value has rawFile property, then it is a file
