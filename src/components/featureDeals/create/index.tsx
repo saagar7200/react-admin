345b7c3b66920a7acc662ec2fb7dd1ffb37042e4
sagar <rsaagar7200@gmail.com>
2022-11-29 13:46:52 +0545

    category and sub category added as type in carousel

--- src/components/featureDeals/create/index.tsx
+++ src/components/featureDeals/create/index.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography } from "@material-ui/core";
+import { useState } from "react";
+import { validate } from "../validaton/index";
 import {
   Create,
   required,
@@ -8,48 +10,101 @@ import {
   AutocompleteInput,
   NumberInput,
   ReferenceInput,
+  SelectInput,
 } from "react-admin";
 
-const choices: { id: string; name: string }[] = [
-  { id: "offer", name: "Offer" },
-  { id: "jackpot", name: "Jackpot" },
-];
-
 export const CreateDeals = (props: any) => {
+  const [type, setType] = useState("");
+
+  const handleSelect = (e: any) => {
+    setType(e.target.value);
+  };
+
+  const transform = (data: any) => {
+    if (data.type === "offer")
+      return { ...data, categoryId: null, subCategoryId: null };
+    if (data.type === "category")
+      return { ...data, offerId: null, subCategoryId: null };
+    if (data.type === "subCategory")
+      return { ...data, offerId: null, categoryId: null };
+  };
+
+  const showInput = () => {
+    if (type === "offer") {
+      return (
+        <ReferenceInput
+          variant="outlined"
+          source="offerId"
+          reference="offers"
+          recordRepresentation="name"
+        >
+          <AutocompleteInput variant="outlined" label="Deal Offer" />
+        </ReferenceInput>
+      );
+    }
+    if (type === "category") {
+      return (
+        <ReferenceInput
+          variant="outlined"
+          source="categoryId"
+          reference="categories"
+          recordRepresentation="name"
+        >
+          <AutocompleteInput variant="outlined" label="Category" />
+        </ReferenceInput>
+      );
+    }
+    return (
+      <ReferenceInput
+        variant="outlined"
+        source="subCategoryId"
+        reference="sub-categories"
+        recordRepresentation="name"
+      >
+        <AutocompleteInput variant="outlined" label="Sub Category" />
+      </ReferenceInput>
+    );
+  };
+
   return (
     <div className="create_category_container">
       <Typography className="form_heading" variant="h5">
         Create a deal
       </Typography>
 
-      <Create title=" " {...props} redirect="list">
-        <SimpleForm>
+      <Create title=" " {...props} redirect="list" transform={transform}>
+        <SimpleForm validate={validate} mode="onBlur" reValidateMode="onBlur">
           <Box className="offer_form_wrapper">
-            <AutocompleteInput
-              source="type"
-              validate={required()}
-              variant="outlined"
-              choices={choices}
-            />
             <ReferenceInput
               variant="outlined"
-              source="offerId"
-              reference="offers"
-              recordRepresentation="name"
+              source="type"
+              reference="carousel/type"
             >
-              <AutocompleteInput variant="outlined" label="Deal Offer" />
+              <SelectInput
+                variant="outlined"
+                validate={required()}
+                label=" Deal Type"
+                optionText="type"
+                source="type"
+                optionValue="type"
+                onChange={handleSelect}
+                defaultValue="subCategory"
+                fullWidth
+              />
             </ReferenceInput>
-          </Box>
 
+            {showInput()}
+          </Box>
           <Box className="offer_form_wrapper">
             <ImageInput className="textInput" source="image" label="Image">
               <ImageField source="src" title="carousel" />
             </ImageInput>
             <NumberInput
+              className="margin-top"
               variant="outlined"
-              validate={required()}
               label="Priority"
               source="priority"
+              min={0}
             />
           </Box>
         </SimpleForm>
